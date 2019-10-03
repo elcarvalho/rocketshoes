580305121b01350351db07b3009daa0a7f8743fb
Eric Lessa <lessa.eric@gmail.com>
2019-10-03 11:38:37 -0300

    Estilização do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,61 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/06/HZM-1731-006/HZM-1731-006_detalhe2.jpg?resize=326:*" alt="Tênis Nike Revolution 5 Masculino"/>
+            </td>
+            <td>
+              <strong>Tênis Nike Revolution 5 Masculino</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size="20" color="7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size="20" color="7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size="20" color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,50</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
